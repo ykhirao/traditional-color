bbd74df5d34ac5e9aec74084fde1b4f9f766934d
Yuki H <yuki.dees39th@gmail.com>
2018-04-27 02:11:22 +0900

    init app

---
+++ src/script.js
@@ -0,0 +1,30 @@
+var option = {
+  paletteSize: 10,
+  exclude: ['rgb(0,0,0)', 'rgb(255,255,255)'],
+  success: function(payload) {
+    console.log(payload)
+    console.log(payload.dominant);
+    console.log(payload.secondary);
+    console.log(payload.palette);
+    payload.palette.forEach((color) =>{
+      console.log('%csam', 'background-color:'+color)
+    })
+  }
+}
+
+var image = document.getElementById('image');
+RGBaster.colors(image, option);
+
+document.querySelector("#file").addEventListener("change", function() {
+  console.log("this", this)
+  if (this.files.length > 0) {
+    var reader = new FileReader();
+    reader.readAsDataURL(this.files[0]);
+
+    reader.onload = function() {
+      img = document.querySelector('#image')
+      img.setAttribute('src', reader.result);
+      RGBaster.colors(img, option);
+    }
+  }
+})
